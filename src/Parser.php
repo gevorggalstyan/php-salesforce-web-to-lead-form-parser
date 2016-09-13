9a14a00b75719747c40ca8cfb53e15964f4b535e
Gevorg Galstyan <gevorggalstyan@Gevorgs-MacBook-Pro.local>
2016-09-13 22:21:44 +0300

    Nicer code

--- src/Parser.php
+++ src/Parser.php
@@ -18,12 +18,14 @@ class Parser
             $label = $node->title;
             if (!$label) {
                 if ($index > 1) {
-                    $ppos = strpos(
-                            $html->innertext,
-                            $nodes[$index - 1]->outertext) + strlen($nodes[$index - 1]->outertext);
-                    $len = strpos($html->innertext, $nodes[$index]->outertext) - $ppos;
+                    $ppos = strpos($html->innertext,
+                            $nodes[$index - 1]->outertext) +
+                        strlen($nodes[$index - 1]->outertext);
+                    $len = strpos($html->innertext,
+                            $nodes[$index]->outertext) - $ppos;
                     return trim(preg_replace('/:|\s\s+/', ' ',
-                        str_get_html(substr($html->innertext, $ppos, $len))->plaintext));
+                        str_get_html(substr($html->innertext,
+                            $ppos, $len))->plaintext));
                 }
             } else {
                 return $label;
@@ -38,19 +40,22 @@ class Parser
             throw new \Exception($html_file . ' is not a readable file');
         }
         $html = new \simple_html_dom();
-        $html->load_file('web-to-lead-full.html');
+        $html->load_file($html_file);
         $forms = $html->find('form[method=POST]');
         if (sizeof($forms) == 0) {
-            throw new \Exception($html_file . ' must contain a form with POST method');
+            throw new \Exception($html_file .
+                ' must contain a form with POST method');
         }
         if (sizeof($forms) > 1) {
-            throw new \Exception($html_file . ' must contain only one form');
+            throw new \Exception($html_file .
+                ' must contain only one form');
         }
 
         $form = $forms[0];
 
         if (!isset($form->action)) {
-            throw new \Exception($html_file . ' must contain form with non-empty action attribute');
+            throw new \Exception($html_file .
+                ' must contain form with non-empty action attribute');
         }
 
         $data_structure = [];
@@ -66,11 +71,14 @@ class Parser
                 ];
                 switch ($node->tag) {
                     case 'input':
-                        $data_structure['fields'][$node->name]['label'] = self::get_label($html, $nodes, $index, $node);
+                        $data_structure['fields'][$node->name]['label'] =
+                            self::get_label($html, $nodes, $index, $node);
                         break;
                     case 'select':
-                        $data_structure['fields'][$node->name]['label'] = self::get_label($html, $nodes, $index, $node);
-                        $data_structure['fields'][$node->name]['multiple'] = ($node->multiple == 'multiple');
+                        $data_structure['fields'][$node->name]['label'] =
+                            self::get_label($html, $nodes, $index, $node);
+                        $data_structure['fields'][$node->name]['multiple'] =
+                            ($node->multiple == 'multiple');
                         $options = $node->find('option');
                         $options_array = [];
                         foreach ($options as $option) {
@@ -78,10 +86,12 @@ class Parser
                                 'value' => $option->value,
                                 'text' => $option->innertext];
                         }
-                        $data_structure['fields'][$node->name]['options'] = [$options_array];
+                        $data_structure['fields'][$node->name]['options'] =
+                            [$options_array];
                         break;
                     case 'textarea':
-                        $data_structure['fields'][$node->name]['label'] = self::get_label($html, $nodes, $index, $node);
+                        $data_structure['fields'][$node->name]['label'] =
+                            self::get_label($html, $nodes, $index, $node);
                         break;
                     default:
                         // This cannot happen
